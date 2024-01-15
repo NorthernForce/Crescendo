76b1ace10a542488213a2d15b9196477ee825b48
cml1010101 <cml1010101@gmail.com>
2024-01-14 21:03:39 -0500

    Add Auto Notes from CAD

---
+++ src/main/java/frc/robot/FieldConstants.java
@@ -0,0 +1,39 @@
+package frc.robot;
+
+import edu.wpi.first.math.geometry.Translation3d;
+
+public class FieldConstants
+{
+    public static class NotePositions
+    {
+        /**
+         * All coordinates are blue origin relative (source corner)
+         */
+        /**
+         * AMP TO SOURCE
+         */
+        public static Translation3d[] blueAutoNotes = new Translation3d[] {
+            new Translation3d(2.945, 7.026, 0),
+            new Translation3d(2.945, 5.5785, 0),
+            new Translation3d(2.945, 4.1305, 0)
+        };
+        /**
+         * AMP TO SOURCE
+         */
+        public static Translation3d[] centerAutoNotes = new Translation3d[] {
+            new Translation3d(8.317, 7.496, 0),
+            new Translation3d(8.317, 5.81, 0),
+            new Translation3d(8.317, 4.134, 0),
+            new Translation3d(8.317, 2.457, 0),
+            new Translation3d(8.317, 0.781, 0),
+        };
+        /**
+         * AMP TO SOURCE
+         */
+        public static Translation3d[] redAutoNotes = new Translation3d[] {
+            new Translation3d(13.694, 7.026, 0),
+            new Translation3d(13.694, 5.5785, 0),
+            new Translation3d(13.694, 4.1305, 0)
+        };
+    }
+}
