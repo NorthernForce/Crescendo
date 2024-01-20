3824d0d7020d7892b3486a96c2ad2545fbef1571
cml1010101 <cml1010101@gmail.com>
2024-01-20 15:06:44 -0500

    Add SwerveDrive.java
    There needed to be a fix for the swerve drive pose estimation.

---
+++ src/main/java/frc/robot/subsystems/SwerveDrive.java
@@ -0,0 +1,34 @@
+package frc.robot.subsystems;
+
+import org.northernforce.gyros.NFRGyro;
+import org.northernforce.subsystems.drive.NFRSwerveDrive;
+import org.northernforce.subsystems.drive.swerve.NFRSwerveModule;
+
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj.Timer;
+
+/**
+ * Swerve drive that extends NFRSwerve Drive. Used for vital fixes
+ */
+public class SwerveDrive extends NFRSwerveDrive
+{
+    /**
+     * Creates a new NFRSwerveDrive.
+     * @param config the configuration for the swerve drive.
+     * @param modules the array of modules
+     * @param offsets the array of offsets... this should be equal in length to that of the modules.
+     * @param gyro the imu.
+     */
+    public SwerveDrive(NFRSwerveDriveConfiguration config, NFRSwerveModule[] modules, Translation2d[] offsets,
+            NFRGyro gyro) {
+        super(config, modules, offsets, gyro);
+    }
+    /**
+     * Updates the odometry and pose estimator with module positions.
+     */
+    public void updateOdometry()
+    {
+        poseEstimator.updateWithTime(Timer.getFPGATimestamp(), getRotation(), getPositions());
+        odometry.update(getRotation(), getPositions());
+    }
+}
