5ac704d5788d54affff8aefc9fccb2823e3fd313
paradox <milo.saucier@gmail.com>
2024-01-24 19:54:18 -0500

    add comments

--- src/main/java/frc/robot/commands/SetServo.java
+++ src/main/java/frc/robot/commands/SetServo.java
@@ -3,10 +3,17 @@ package frc.robot.commands;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.Servo;
 import edu.wpi.first.wpilibj2.command.Command;
-
+/**
+ * this is to control the servo
+ */
 public class SetServo extends Command {
     protected final Servo servo;
     protected final Rotation2d angle;
+    /**
+     * this initilizes the servo fine motor control
+     * @param servo the servo to control
+     * @param angle sets the angle of servo
+     */
     public SetServo(Servo servo, Rotation2d angle)
     {
         this.servo = servo;
@@ -16,6 +23,5 @@ public class SetServo extends Command {
     public void initialize() 
     {
         servo.setAngle(angle.getDegrees());
-
     }
 }
