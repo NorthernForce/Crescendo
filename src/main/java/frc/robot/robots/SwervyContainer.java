171f27f77db505217a0f8cef2f25904c0ea8d065
cml1010101 <115354809+cml1010101@users.noreply.github.com>
2024-01-26 16:19:28 -0500

    Create OrangePi Subsystem (#27)
    * Create OrangePi.java
    
    * Change SwervyContainer.java to add OrangePi subsystem
    
    * More support for note and tag tracking
    
    * Show robot pose on field
    
    * Add json files for simulation
    
    * Get rid of RobotContainer.java
    
    * Add support for GenericHIDs in SwervyContainer.java
    
    * Change NoteDetector camera system to TargetCamera
    
    * Switch to TargetCamera class in SwervyContainer.java
    
    * Use dtheta for odometry and imu
    
    * Add SwerveDrive.java
    
    There needed to be a fix for the swerve drive pose estimation.
    
    * Change OrangePi.java to reflect changes to nfr_bridge_node
    
    * Change SwervyContainer.java to reflect changes to OrangePi.java
    
    * Get rid of sim files

--- src/main/java/frc/robot/robots/SwervyContainer.java
+++ src/main/java/frc/robot/robots/SwervyContainer.java
@@ -5,7 +5,6 @@ import org.northernforce.commands.NFRSwerveDriveCalibrate;
 import org.northernforce.commands.NFRSwerveDriveStop;
 import org.northernforce.commands.NFRSwerveDriveWithJoystick;
 import org.northernforce.commands.NFRSwerveModuleSetState;
-import org.northernforce.subsystems.drive.NFRSwerveDrive;
 import org.northernforce.subsystems.drive.NFRSwerveDrive.NFRSwerveDriveConfiguration;
 import org.northernforce.subsystems.drive.swerve.NFRSwerveModule;
 import org.northernforce.util.NFRRobotContainer;
@@ -13,20 +12,33 @@ import org.northernforce.util.NFRRobotContainer;
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.Pair;
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.robot.utils.SwerveModuleHelpers;
 import frc.robot.gyros.NFRPigeon2;
+import frc.robot.subsystems.OrangePi;
+import frc.robot.subsystems.SwerveDrive;
+import frc.robot.subsystems.OrangePi.OrangePiConfiguration;
+import frc.robot.subsystems.OrangePi.PoseSupplier;
+import frc.robot.subsystems.OrangePi.TargetCamera;
 
 public class SwervyContainer implements NFRRobotContainer
 {
-    protected final NFRSwerveDrive drive;
+    protected final SwerveDrive drive;
     protected final NFRSwerveModuleSetState[] setStateCommands;
+    protected final OrangePi orangePi;
+    protected final Field2d field;
+    protected final TargetCamera aprilTagCamera, noteDetectorCamera;
+    protected final NFRPigeon2 gyro;
+    protected final PoseSupplier aprilTagSupplier;
     public SwervyContainer()
     {
         NFRSwerveModule[] modules = new NFRSwerveModule[] {
@@ -41,15 +53,24 @@ public class SwervyContainer implements NFRRobotContainer
             new Translation2d(-0.581025, 0.581025),
             new Translation2d(-0.581025, -0.581025)
         };
-        NFRPigeon2 gyro = new NFRPigeon2(13);
-        drive = new NFRSwerveDrive(new NFRSwerveDriveConfiguration("drive"), modules, offsets, gyro);
+        gyro = new NFRPigeon2(13);
+        drive = new SwerveDrive(new NFRSwerveDriveConfiguration("drive"), modules, offsets, gyro);
         setStateCommands = new NFRSwerveModuleSetState[] {
             new NFRSwerveModuleSetState(modules[0], 0, false),
             new NFRSwerveModuleSetState(modules[1], 0, false),
             new NFRSwerveModuleSetState(modules[2], 0, false),
             new NFRSwerveModuleSetState(modules[3], 0, false)
         };
+        orangePi = new OrangePi(new OrangePiConfiguration("orange pi", "xavier"));
         Shuffleboard.getTab("General").add("Calibrate Swerve", new NFRSwerveDriveCalibrate(drive));
+        Shuffleboard.getTab("General").addBoolean("Xavier Connected", orangePi::isConnected);
+        field = new Field2d();
+        Shuffleboard.getTab("General").add("Field", field);
+        noteDetectorCamera = orangePi.new TargetCamera("usb_cam2");
+        aprilTagCamera = orangePi.new TargetCamera("usb_cam1");
+        aprilTagSupplier = orangePi.new PoseSupplier("usb_cam1", estimate -> {
+            drive.addVisionEstimate(estimate.getSecond(), estimate.getFirst());
+        });
     }
     @Override
     public void bindOI(GenericHID driverHID, GenericHID manipulatorHID)
@@ -66,6 +87,17 @@ public class SwervyContainer implements NFRRobotContainer
             new JoystickButton(driverController, XboxController.Button.kY.value)
                 .onTrue(new NFRSwerveDriveStop(drive, setStateCommands, true));
         }
+        else
+        {
+            drive.setDefaultCommand(new NFRSwerveDriveWithJoystick(drive, setStateCommands,
+                () -> -MathUtil.applyDeadband(driverHID.getRawAxis(1), 0.1, 1),
+                () -> -MathUtil.applyDeadband(driverHID.getRawAxis(0), 0.1, 1),
+                () -> -MathUtil.applyDeadband(driverHID.getRawAxis(5), 0.1, 1), true, true));
+            new JoystickButton(driverHID, XboxController.Button.kB.value)
+                .onTrue(Commands.runOnce(drive::clearRotation, drive));
+            new JoystickButton(driverHID, XboxController.Button.kY.value)
+                .onTrue(new NFRSwerveDriveStop(drive, setStateCommands, true));
+        }
     }
     @Override
     public Map<String, Command> getAutonomousOptions()
@@ -75,7 +107,7 @@ public class SwervyContainer implements NFRRobotContainer
     @Override
     public Map<String, Pose2d> getStartingLocations()
     {
-        return Map.of("Simple Starting Location", new Pose2d());
+        return Map.of("Simple Starting Location", new Pose2d(5, 5, Rotation2d.fromDegrees(0)));
     }
     @Override
     public Pair<String, Command> getDefaultAutonomous()
@@ -86,5 +118,14 @@ public class SwervyContainer implements NFRRobotContainer
     public void setInitialPose(Pose2d pose)
     {
         drive.resetPose(pose);
+    }
+    @Override
+    public void periodic()
+    {
+        var chassisSpeeds = drive.getChassisSpeeds();
+        orangePi.setOdometry(drive.getOdometryPose(), chassisSpeeds.vxMetersPerSecond, chassisSpeeds.vyMetersPerSecond,
+            Rotation2d.fromRadians(chassisSpeeds.omegaRadiansPerSecond), Timer.getFPGATimestamp());
+        orangePi.setPose(drive.getEstimatedPose(), Timer.getFPGATimestamp());
+        field.setRobotPose(drive.getEstimatedPose());
     }
 }
---
+++ src/main/java/frc/robot/subsystems/OrangePi.java
@@ -0,0 +1,231 @@
+package frc.robot.subsystems;
+
+import java.util.EnumSet;
+import java.util.function.Consumer;
+
+import org.northernforce.subsystems.NFRSubsystem;
+
+import edu.wpi.first.math.Pair;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Twist2d;
+import edu.wpi.first.networktables.BooleanPublisher;
+import edu.wpi.first.networktables.DoubleArraySubscriber;
+import edu.wpi.first.networktables.DoublePublisher;
+import edu.wpi.first.networktables.DoubleSubscriber;
+import edu.wpi.first.networktables.IntegerArraySubscriber;
+import edu.wpi.first.networktables.IntegerPublisher;
+import edu.wpi.first.networktables.IntegerSubscriber;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.networktables.NetworkTableEvent.Kind;
+
+/**
+ * This is a subsystem for the Orange Pi 5+ that runs nfr_ros.
+ */
+public class OrangePi extends NFRSubsystem
+{
+    /**
+     * This is the configuration class for the OrangePi subsystem.
+     */
+    public static class OrangePiConfiguration extends NFRSubsystemConfiguration
+    {
+        protected String tableName;
+        /**
+         * Create a new OrangePiConfiguration.
+         * @param name the name of the subsystem
+         */
+        public OrangePiConfiguration(String name)
+        {
+            super(name);
+        }
+        /**
+         * Create a new OrangePiConfiguration.
+         * @param name the name of the subsystem
+         * @param tableName the name of the table where the orange pi publishes its information
+         */
+        public OrangePiConfiguration(String name, String tableName)
+        {
+            super(name);
+            this.tableName = tableName;
+        }
+        /**
+         * With table name
+         * @param tableName the name of the table where the orange pi publishes its information
+         * @return this
+         */
+        public OrangePiConfiguration withTableName(String tableName)
+        {
+            this.tableName = tableName;
+            return this;
+        }
+    }
+    protected final NetworkTable table;
+    protected final NetworkTable odometryTable;
+    protected final DoublePublisher odometryX, odometryY, odometryTheta, odometryDeltaX, odometryDeltaY, odometryDeltaTheta;
+    protected final IntegerPublisher odometryStamp;
+    protected final NetworkTable targetPoseTable;
+    protected final DoublePublisher targetPoseX, targetPoseY, targetPoseTheta;
+    protected final IntegerPublisher targetPoseStamp;
+    protected final BooleanPublisher targetPoseCancel;
+    protected final NetworkTable cmdVelTable;
+    protected final DoubleSubscriber cmdVelX, cmdVelY, cmdVelTheta;
+    protected final NetworkTable poseTable;
+    protected final DoublePublisher poseX, poseY, poseTheta;
+    protected final IntegerPublisher poseStamp;
+    /**
+     * Create a new orange pi.
+     * @param config
+     */
+    public OrangePi(OrangePiConfiguration config)
+    {
+        super(config);
+        table = NetworkTableInstance.getDefault().getTable(config.tableName);
+        odometryTable = table.getSubTable("odometry");
+        odometryX = odometryTable.getDoubleTopic("x").publish();
+        odometryY = odometryTable.getDoubleTopic("y").publish();
+        odometryTheta = odometryTable.getDoubleTopic("theta").publish();
+        odometryDeltaX = odometryTable.getDoubleTopic("vx").publish();
+        odometryDeltaY = odometryTable.getDoubleTopic("vy").publish();
+        odometryDeltaTheta = odometryTable.getDoubleTopic("vtheta").publish();
+        odometryStamp = odometryTable.getIntegerTopic("stamp").publish();
+        targetPoseTable = table.getSubTable("target_pose");
+        targetPoseX = targetPoseTable.getDoubleTopic("x").publish();
+        targetPoseY = targetPoseTable.getDoubleTopic("y").publish();
+        targetPoseTheta = targetPoseTable.getDoubleTopic("theta").publish();
+        targetPoseStamp = targetPoseTable.getIntegerTopic("stamp").publish();
+        targetPoseCancel = targetPoseTable.getBooleanTopic("cancel").publish();
+        cmdVelTable = table.getSubTable("cmd_vel");
+        cmdVelX = cmdVelTable.getDoubleTopic("x").subscribe(0);
+        cmdVelY = cmdVelTable.getDoubleTopic("y").subscribe(0);
+        cmdVelTheta = cmdVelTable.getDoubleTopic("theta").subscribe(0);
+        poseTable = table.getSubTable("pose");
+        poseX = poseTable.getDoubleTopic("x").publish();
+        poseY = poseTable.getDoubleTopic("y").publish();
+        poseTheta = poseTable.getDoubleTopic("theta").publish();
+        poseStamp = poseTable.getIntegerTopic("stamp").publish();
+    }
+    /**
+     * Sets the odometry
+     * @param deltaX in m/s
+     * @param deltaY in m/s
+     * @param deltaTheta change in angle
+     * @param stamp in seconds
+     */
+    public void setOdometry(Pose2d pose, double deltaX, double deltaY, Rotation2d deltaTheta, double stamp)
+    {
+        odometryX.set(pose.getX());
+        odometryY.set(pose.getY());
+        odometryTheta.set(pose.getRotation().getRadians());
+        odometryDeltaX.set(deltaX);
+        odometryDeltaY.set(deltaY);
+        odometryDeltaTheta.set(deltaTheta.getRadians());
+        odometryStamp.set((long)(stamp * 1e9));
+    }
+    /**
+     * Sends a target pose
+     * @param pose the target pose
+     * @param stamp in seconds
+     */
+    public void sendTargetPose(Pose2d pose, double stamp)
+    {
+        targetPoseX.set(pose.getX());
+        targetPoseY.set(pose.getY());
+        targetPoseTheta.set(pose.getRotation().getRadians());
+        targetPoseStamp.set((long)(stamp * 1e9));
+    }
+    /**
+     * Cancels a target pose
+     */
+    public void cancelTargetPose()
+    {
+        targetPoseCancel.set(true);
+    }
+    /**
+     * Gets the command velocity from nav2
+     * @return dx, dy, and dtheta
+     */
+    public Twist2d getCommandVelocity()
+    {
+        return new Twist2d(cmdVelX.get(), cmdVelY.get(), cmdVelTheta.get());
+    }
+    /**
+     * Sets the map -> base_link pose
+     * @param pose in meters
+     * @param stamp in seconds
+     */
+    public void setPose(Pose2d pose, double stamp)
+    {
+        poseX.set(pose.getX());
+        poseY.set(pose.getY());
+        poseTheta.set(pose.getRotation().getRadians());
+        poseStamp.set((long)(stamp * 1e9));
+    }
+    /**
+     * Checks to see if the orange pi is connected
+     * @return whether the orange pi is connected
+     */
+    public boolean isConnected()
+    {
+        boolean connectionFound = false;
+        for (var connection : NetworkTableInstance.getDefault().getConnections())
+        {
+            if (connection.remote_id.startsWith("xavier"))
+            {
+                connectionFound = true;
+            }
+        }
+        return connectionFound;
+    }
+    public static record TargetDetection(double area, double tx, double ty, double pitch, double yaw, int fiducialID)
+    {
+    }
+    public class TargetCamera
+    {
+        protected final NetworkTable table;
+        protected final DoubleArraySubscriber area;
+        protected final DoubleArraySubscriber tx;
+        protected final DoubleArraySubscriber ty;
+        protected final DoubleArraySubscriber yaw;
+        protected final DoubleArraySubscriber pitch;
+        protected final IntegerArraySubscriber fiducialID;
+        protected final IntegerArraySubscriber stamp;
+        public TargetCamera(String name)
+        {
+            table = OrangePi.this.table.getSubTable(name);
+            area = table.getDoubleArrayTopic("area").subscribe(new double[] {});
+            tx = table.getDoubleArrayTopic("tx").subscribe(new double[] {});
+            ty = table.getDoubleArrayTopic("ty").subscribe(new double[] {});
+            yaw = table.getDoubleArrayTopic("yaw").subscribe(new double[] {});
+            pitch = table.getDoubleArrayTopic("pitch").subscribe(new double[] {});
+            fiducialID = table.getIntegerArrayTopic("fiducial_id").subscribe(new long[] {});
+            stamp = table.getIntegerArrayTopic("stamp").subscribe(new long[] {});
+        }
+        public TargetDetection[] getDetections()
+        {
+            TargetDetection[] detections = new TargetDetection[stamp.get().length];
+            for (int i = 0; i < stamp.get().length; i++)
+            {
+                detections[i] = new TargetDetection(area.get()[i], tx.get()[i], ty.get()[i], yaw.get()[i], pitch.get()[i], (int)fiducialID.get()[i]);
+            }
+            return detections;
+        }
+    }
+    public class PoseSupplier
+    {
+        protected final NetworkTable table;
+        protected final DoubleSubscriber x, y, theta;
+        protected final IntegerSubscriber stamp;
+        public PoseSupplier(String name, Consumer<Pair<Pose2d, Double>> consumer)
+        {
+            table = OrangePi.this.table.getSubTable("poses").getSubTable(name);
+            x = table.getDoubleTopic("x").subscribe(0);
+            y = table.getDoubleTopic("y").subscribe(0);
+            theta = table.getDoubleTopic("theta").subscribe(0);
+            stamp = table.getIntegerTopic("stamp").subscribe(0);
+            NetworkTableInstance.getDefault().addListener(stamp, EnumSet.of(Kind.kValueAll), event -> {
+                consumer.accept(Pair.of(new Pose2d(x.get(), y.get(), Rotation2d.fromRadians(theta.get())), (double)stamp.get() / 1e9));
+            });
+        }
+    }
+}
