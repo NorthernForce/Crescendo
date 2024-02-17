6236ee0412c83cc4864c34839f00abfe08b25222
Harlan Haller <harlanhaller06@gmail.com>
2024-02-17 15:44:59 -0500

    working and tested pid constants

--- src/main/java/frc/robot/utils/SwerveModuleHelpers.java
+++ src/main/java/frc/robot/utils/SwerveModuleHelpers.java
@@ -41,10 +41,10 @@ public class SwerveModuleHelpers
         driveConfig.CurrentLimits.SupplyTimeThreshold = 0.5;
         driveConfig.CurrentLimits.SupplyCurrentLimitEnable = true;
         driveConfig.Slot0.kP = Mk3SwerveConstants.kDriveP;
-        driveConfig.Slot1.kS = 0.001;//TODO set this val. THESE ARE WILD SHOT IN THE DARK GUESSES. Exercise caution when testing.
+        driveConfig.Slot1.kS = 0.01;//TODO set this val. THESE ARE WILD SHOT IN THE DARK GUESSES. Exercise caution when testing.
         driveConfig.Slot1.kV = 0.01;//TODO set this val
         driveConfig.Slot1.kP = 0.03; //TODO set this val
-        driveConfig.Slot1.kI = 0; //TODO set this val
+        driveConfig.Slot1.kI = 0.02; //TODO set this val
         driveConfig.Slot1.kD = 0; //TODO set this val. Also put these values in the other factory when ready and confirmed.
         driveConfig.MotorOutput.NeutralMode = NeutralModeValue.Brake;
         driveConfig.MotorOutput.DutyCycleNeutralDeadband = 0.1;
