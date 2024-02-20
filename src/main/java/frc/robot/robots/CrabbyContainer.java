1c81eea921fb38c4fb5a0e5ec30b846659cb4a77
Paradox0100 <125032753+Paradox0100@users.noreply.github.com>
2024-02-20 18:45:34 -0500

    add drive to CrabbyContainer (#102)
    * add drive to CrabbyContainer
    
    * cleaning it up

--- src/main/java/frc/robot/robots/CrabbyContainer.java
+++ src/main/java/frc/robot/robots/CrabbyContainer.java
@@ -3,6 +3,12 @@ package frc.robot.robots;
 import java.util.List;
 import java.util.Map;
 
+import org.northernforce.commands.NFRSwerveDriveCalibrate;
+import org.northernforce.commands.NFRSwerveDriveStop;
+import org.northernforce.commands.NFRSwerveDriveWithJoystick;
+import org.northernforce.commands.NFRSwerveModuleSetState;
+import org.northernforce.subsystems.drive.NFRSwerveDrive.NFRSwerveDriveConfiguration;
+import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.Pair;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
@@ -11,22 +17,39 @@ import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import frc.robot.constants.CrabbyConstants;
 import frc.robot.subsystems.OrangePi;
 import frc.robot.subsystems.OrangePi.OrangePiConfiguration;
 import frc.robot.subsystems.OrangePi.PoseSupplier;
 import frc.robot.subsystems.OrangePi.TargetCamera;
+import frc.robot.subsystems.SwerveDrive;
 import frc.robot.utils.AutonomousRoutine;
 import frc.robot.utils.RobotContainer;
 
 public class CrabbyContainer implements RobotContainer
 {
-  
+    protected final SwerveDrive drive;
+    protected final NFRSwerveModuleSetState[] setStateCommands;
+
     protected final OrangePi orangePi;
     protected final TargetCamera aprilTagCamera, noteDetectorCamera;
     protected final PoseSupplier aprilTagSupplier;
+    protected final CrabbyMap map;
     public CrabbyContainer()
     {
+        map = new CrabbyMap();
+        
+        drive = new SwerveDrive(new NFRSwerveDriveConfiguration("drive"), map.modules, CrabbyConstants.Drive.offsets, map.gyro);
+        setStateCommands = new NFRSwerveModuleSetState[] {
+            new NFRSwerveModuleSetState(map.modules[0], 0, false),
+            new NFRSwerveModuleSetState(map.modules[1], 0, false),
+            new NFRSwerveModuleSetState(map.modules[2], 0, false),
+            new NFRSwerveModuleSetState(map.modules[3], 0, false)
+        };
+
         orangePi = new OrangePi(new OrangePiConfiguration("orange pi", "xavier"));
+        Shuffleboard.getTab("General").add("Calibrate Swerve", new NFRSwerveDriveCalibrate(drive).ignoringDisable(true));
         Shuffleboard.getTab("General").addBoolean("Xavier Connected", orangePi::isConnected);
         noteDetectorCamera = orangePi.new TargetCamera("usb_cam2");
         aprilTagCamera = orangePi.new TargetCamera("usb_cam1");
@@ -37,9 +60,26 @@ public class CrabbyContainer implements RobotContainer
     {
         if (driverHID instanceof XboxController)
         {
+            XboxController driverController = (XboxController)driverHID;
+            drive.setDefaultCommand(new NFRSwerveDriveWithJoystick(drive, setStateCommands,
+                () -> -MathUtil.applyDeadband(driverController.getLeftY(), 0.1, 1),
+                () -> -MathUtil.applyDeadband(driverController.getLeftX(), 0.1, 1),
+                () -> -MathUtil.applyDeadband(driverController.getRightX(), 0.1, 1), true, true));
+            new JoystickButton(driverController, XboxController.Button.kB.value)
+                .onTrue(Commands.runOnce(drive::clearRotation, drive));
+            new JoystickButton(driverController, XboxController.Button.kY.value)
+                .onTrue(new NFRSwerveDriveStop(drive, setStateCommands, true));
         }
         else
         {
+            drive.setDefaultCommand(new NFRSwerveDriveWithJoystick(drive, setStateCommands,
+                () -> -MathUtil.applyDeadband(driverHID.getRawAxis(1), 0.1, 1),
+                () -> -MathUtil.applyDeadband(driverHID.getRawAxis(0), 0.1, 1),
+                () -> -MathUtil.applyDeadband(driverHID.getRawAxis(5), 0.1, 1), true, true));
+            new JoystickButton(driverHID, XboxController.Button.kB.value)
+                .onTrue(Commands.runOnce(drive::clearRotation, drive));
+            new JoystickButton(driverHID, XboxController.Button.kY.value)
+                .onTrue(new NFRSwerveDriveStop(drive, setStateCommands, true));
         }
     }
     @Override
