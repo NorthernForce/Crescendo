bafc972da178431169b36758983000d1d9a5ef7f
Kasey Nanovic <148725607+kcnan@users.noreply.github.com>
2024-02-21 16:38:10 -0500

    modified it to be put in CrabbyMap

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -8,12 +8,13 @@ import edu.wpi.first.util.sendable.SendableBuilder;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Intake extends SubsystemBase {
-    private final int PRIMARY_MOTOR_ID = 0; // TODO: set actual motor IDs
-    private final int[] FOLLOWER_MOTOR_IDS = {1, 2, 3};
     private final NFRSparkMax motors;
-    /** Creates a new Intake subsystem */
-    public Intake() {
-        this.motors = new NFRSparkMax(MotorType.kBrushless, PRIMARY_MOTOR_ID, FOLLOWER_MOTOR_IDS);
+    /** Creates a new Intake subsystem
+     * @param primaryMotor the primary motor of the intake (doesn't matter which one)
+     * @param followerMotors the follower motors
+    */
+    public Intake(int primaryMotor, int... followerMotors) {
+        this.motors = new NFRSparkMax(MotorType.kBrushless, primaryMotor, followerMotors);
     }
     /**
      * Runs the intake motors
