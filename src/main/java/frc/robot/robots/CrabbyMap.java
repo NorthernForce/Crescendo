d39ab1f0952e1fdd923f12d863161f10121d26f2
Zack Bourgoin <whiteheatzack@gmail.com>
2024-02-21 20:30:01 -0500

    Set indexer motor id and beam break pin

--- src/main/java/frc/robot/robots/CrabbyMap.java
+++ src/main/java/frc/robot/robots/CrabbyMap.java
@@ -19,7 +19,7 @@ public class CrabbyMap {
         SwerveModuleHelpers.createMk4iL3("Back Right", 4, 8, 12, true, "drive")
     };
     public final NFRGyro gyro = new NFRPigeon2(13);
-    public final Intake intake = new Intake(0, 1, 2, 3); // TODO: set actual intake motors
-    public final NFRSparkMax indexerMotor = new NFRSparkMax(MotorType.kBrushless, 4); // TODO: set actual indexer motor id
-    public final NFRBeamBreak indexerBeamBreak = new NFRBeamBreak(0); // TODO: set actual indexer beam break pin number
+    public final Intake intake = new Intake(17, 18, 19, 20); // TODO: set actual intake motors
+    public final NFRSparkMax indexerMotor = new NFRSparkMax(MotorType.kBrushless, 16);
+    public final NFRBeamBreak indexerBeamBreak = new NFRBeamBreak(0);
 }
