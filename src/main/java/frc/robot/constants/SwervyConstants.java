7b8dea95e71094b72d667f1bc1cdde6f712bc249
cml1010101 <cml1010101@gmail.com>
2024-02-22 15:42:19 -0500

    Move pid controller around

--- src/main/java/frc/robot/constants/SwervyConstants.java
+++ src/main/java/frc/robot/constants/SwervyConstants.java
@@ -2,12 +2,15 @@ package frc.robot.constants;
 
 import org.northernforce.subsystems.drive.NFRSwerveDrive.NFRSwerveDriveConfiguration;
 
+import com.pathplanner.lib.controllers.PPHolonomicDriveController;
+import com.pathplanner.lib.util.PIDConstants;
+
 import edu.wpi.first.math.geometry.Translation2d;
 import frc.robot.subsystems.OrangePi.OrangePiConfiguration;
 
 public class SwervyConstants
 {
-    public static class Drive
+    public static class DriveConstants
     {
         /** X/Y offsets of each swerve module front left -> back right */
         public static final Translation2d[] offsets = new Translation2d[] {
@@ -17,8 +20,13 @@ public class SwervyConstants
             new Translation2d(-0.581025, -0.581025)
         };
         public static final NFRSwerveDriveConfiguration config = new NFRSwerveDriveConfiguration("drive");
+        public static final PPHolonomicDriveController holonomicDriveController = new PPHolonomicDriveController(
+            new PIDConstants(1, 0, 0.2), // X/Y pid constants
+            new PIDConstants(4, 0, 0), // Rotational pid constants
+            6, // Max Module Speed
+            offsets[0].getDistance(new Translation2d())); // Distance from center
     }
-    public static class OrangePi
+    public static class OrangePiConstants
     {
         public static final OrangePiConfiguration config = new OrangePiConfiguration("orangepi", "xavier");
     }
