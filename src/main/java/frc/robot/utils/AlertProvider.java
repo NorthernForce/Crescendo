01b30abc29eeefed0aad04cf4a9b2186835c446d
cml1010101 <cml1010101@gmail.com>
2024-02-23 15:52:45 -0500

    Rework the alert system

---
+++ src/main/java/frc/robot/utils/AlertProvider.java
@@ -0,0 +1,13 @@
+package frc.robot.utils;
+
+import java.util.List;
+
+import frc.robot.dashboard.Dashboard.Alert;
+
+public interface AlertProvider {
+    /**
+     * Returns a list of alerts that can be checked from time to time to see whether they are valid or not
+     * @return a list of alerts that may need to be displayed on a dashboard
+     */
+    public List<Alert> getPossibleAlerts();
+}
