d9aa3c3598fbc524a7af65b1e96e3e4f27e56190
Paradox0100 <125032753+Paradox0100@users.noreply.github.com>
2024-02-22 19:28:51 -0500

    Path planner milo (#121)
    * add set SetServo.java
    
    * add comments
    
    * msg this auto S2.T
    
    * pls work
    
    * fixed S2.T path
    
    * fixed fixed the auto path S2.T
    
    * fixed for hopefully the final time
    
    * finaly up to Connor's standards
    
    * pls work

--- src/main/java/frc/robot/commands/auto/Autos.java
+++ src/main/java/frc/robot/commands/auto/Autos.java
@@ -89,6 +89,11 @@ public class Autos
         return S1CSV1.getRoutine(drive, setStateCommands, poseSupplier, controller,
             () -> DriverStation.getAlliance().orElse(Alliance.Red) == Alliance.Red);
     }
+    public static AutonomousRoutine getS2T(NFRSwerveDrive drive, NFRSwerveModuleSetState[] setStateCommands, Supplier<Pose2d> poseSupplier, PPHolonomicDriveController controller)
+    {
+        return S2T.getRoutine(drive, setStateCommands, poseSupplier, controller,
+            () -> DriverStation.getAlliance().orElse(Alliance.Red) == Alliance.Red);
+    }
     /**
      * Gets the list of autonomous routines.
      * Includes S1.CS.V1
@@ -106,7 +111,8 @@ public class Autos
             getS1LV1(drive, setStateCommands, poseSupplier, controller), 
             getS1LV2(drive, setStateCommands, poseSupplier, controller),
             getS2CV1(drive, setStateCommands, poseSupplier, controller),
-            getS2CV2(drive, setStateCommands, poseSupplier, controller)
+            getS2CV2(drive, setStateCommands, poseSupplier, controller),
+            getS2T(drive, setStateCommands, poseSupplier, controller)
         );
     }
 }
