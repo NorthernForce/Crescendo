6da08980610a7ca798f00b262dc54ad51c288586
FRCTeam172 <team172@northernforcerobotics.org>
2024-02-28 19:42:16 -0500

    added arm reset

---
+++ src/main/java/frc/robot/commands/NFRResetWristCommand.java
@@ -0,0 +1,21 @@
+package frc.robot.commands;
+
+import org.northernforce.subsystems.arm.NFRRotatingArmJoint;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj2.command.Command;
+
+public class NFRResetWristCommand extends Command {
+    NFRRotatingArmJoint m_wrist;
+    public NFRResetWristCommand(NFRRotatingArmJoint wrist)
+    {
+        m_wrist = wrist;
+    }
+    @Override public void initialize()
+    {
+        addRequirements(m_wrist);
+        m_wrist.getController().getAbsoluteEncoder().get().setAbsoluteOffset(Rotation2d.fromDegrees(25).getRotations());
+    }
+
+
+}
