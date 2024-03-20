89d93bd567dff6835a66faf6f2a0c9c7357f7d42
Daboss-1 <139522291+Daboss-1@users.noreply.github.com>
2024-03-20 19:07:35 +0000

    inverse matrix functions added

--- src/main/java/frc/robot/utils/PolyReg.java
+++ src/main/java/frc/robot/utils/PolyReg.java
@@ -6,10 +6,6 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 import org.opencv.core.Point;
 
@@ -81,54 +77,89 @@ public class PolyReg implements TargetingCalculator{
 
         return origMatrices[0]; // TODO CHANGE THIS!!!
     }
-    // public double[][] findInverseMatrices(double[][] current)
-    // {
-    //     double[][] result = new double[current.length][current[0].length];
-    //     double multAmount = 1.0 / ((Supplier<Double>) () -> 
-    //     {
-            
-    //     }).get();
-
-    // }
-    // public double[][] matriceOfMinors(double[][] current)
-    // {
-
-    // }
-    public double determinantOfMatrix(double[][] current)
+    public static double[][] findInverseMatrices(double[][] current)
     {
-        Map<Double,double[][]> hashResult = new HashMap<>();
-        double[][] result = new double[current.length][current.length];
+        double[][] original = adjugateMatrix(current);
+        double[][] result = new double[current.length][current[0].length];
+        double multAmount = 1.0 / ((Supplier<Double>) () -> 
+        {
+            return determinantOfMatrix(current,1);
+        }).get();
+        for(int y = 0; y < result.length; y++)
+        {
+            for(int x = 0; x < result[0].length; x++)
+            {
+                original[y][x]*=multAmount;
+            }
+        }
+        return original;
+
+    }
+    public static double[][] adjugateMatrix(double[][] current)
+    {
+        double[][] original = matrixOfCofactors(current);
+        double[][] result = new double[current.length][current[0].length];
+        for(int y = 0; y < current.length; y++)
+        {
+            for(int x = 0; x < current[0].length; x++)
+            {
+                result[y][x] = original[x][y];
+            }
+        }
+        return result;
+    }
+    public static double[][] matrixOfCofactors(double[][] current)
+    {
+        int pos = 0;
+        double[][] result = new double[current.length][current[0].length];
+        for(int y = 0; y < current.length; y++)
+        {
+            for(int x = 0; x < current[0].length; x++)
+            {
+            //     for(int yy = 0; yy < externalMiniOperator(current, x, y).length; y++)
+            //     {
+            //     for(int xx = 0; xx < externalMiniOperator(current, x, y)[0].length; xx++)
+            //     {
+            //         System.out.println(externalMiniOperator(current, x, y)[yy][xx]);
+            //     }
+            //     System.out.println("\n");
+            // }
+                result[y][x] = determinantOfMatrix(externalMiniOperator(current, x, y), 1) * -1 *(pos % 2 * 2 -1);
+                pos++;
+            }
+        }
+        return result;
+    }
+    public static double determinantOfMatrix(double[][] current, double multBy)
+    {   
+        if(current.length > 2)
+        {
+        double[][] result = new double[current.length][current[0].length];
         double[][] tempLittleMatrixes = new double[current.length-1][current[0].length-1];
+
         double tempResult = 0;
         int cx = 0;
         int cy = 0;
         int colDown = 0;
         int rowDown = 0;
-        double origTempResult = tempResult;
-
+        int changeX = 0;
+        int changed = 0;
         int pos = 0;
-        for(int mx = 0; mx < current[0].length; mx++)
+        for(int mx = 0; mx < result[0].length; mx++)
         {
-            cx++;
-            if(cx == points.size())
-            {
-                cx = 0;
-                cy++;
-            }
             rowDown = 0;
             colDown = 0;
-            tempResult = 0;
-            for(int y = 1; y < current.length; y++)
+            for(int y = 1; y < result.length; y++)
             {
 
                 
-                for(int x = 0; x < current[0].length; x++)
+                for(int x = 0; x < result[0].length; x++)
                 {
                     if(x != mx)
                     {
                         tempLittleMatrixes[colDown][rowDown] = current[y][x];
                         rowDown++;
-
+                        changed++;
                         if(rowDown == tempLittleMatrixes[0].length)
                         {
                             rowDown = 0;
@@ -136,22 +167,50 @@ public class PolyReg implements TargetingCalculator{
                         }
                         
                     }
-
+                    else
+                    {
+                        changeX = 1;
+                    }
                 }   
 
             }
-            tempResult+=current[0][mx]*(pos%2 == 0 ? 1 : -1)*(tempLittleMatrixes.length == 2 ? (tempLittleMatrixes[0][0]*tempLittleMatrixes[1][1]-tempLittleMatrixes[0][1]*tempLittleMatrixes[1][0]) : determinantOfMatrix(tempLittleMatrixes));
-            if(current.length == points.size())
-            {
-                result[cy][cx] = tempResult-origTempResult;
-            }
-            origTempResult = tempResult;
+            tempResult+=current[0][mx]*-1*(pos%2*2-1)*determinantOfMatrix(tempLittleMatrixes,1);
             pos++;
         }
-        double[][] noBottomFeeder =result;
-        return tempResult;
+        double noBottomFeeder =tempResult;
+        return noBottomFeeder;
+        }
+        else
+        {
+            return (current[0][0]*current[1][1]-current[0][1]*current[1][0]) * multBy;
+        }
     }
+    public static double[][] externalMiniOperator(double[][] current, int mx, int my)
+    {
+        int rowDown = 0;
+        int colDown = 0;
+        double[][] tempLittleMatrixes = new double[current.length-1][current[0].length-1];
+            for(int y = 0; y < current.length; y++)
+            {
+                
+                for(int x = 0; x < current[0].length; x++)
+                {
+                    if(x != mx && y != my)
+                    {
+                        tempLittleMatrixes[colDown][rowDown] = current[y][x];
+                        rowDown++;
+                        if(rowDown == tempLittleMatrixes[0].length)
+                        {
+                            rowDown = 0;
+                            colDown++;
+                        }
+                        
+                    }
+                }   
 
+            }
+            return tempLittleMatrixes;
+    }
     public VariableOperator evalSkelequation(double x)
     {
         VariableOperator operator = (operands) -> {
--- src/main/java/frc/robot/utils/testPoly.java
+++ src/main/java/frc/robot/utils/testPoly.java
@@ -2,9 +2,18 @@ package frc.robot.utils;
 
 public class testPoly {
     public static void main(String[] args) {
-        double[][] test = {{1,2,3},{4,5,6},{7,2,9}};
-        double result = PolyReg.determinantOfMatrix(test);
-        System.out.println(result);
+        double[][] test = {{5,4,8,6,9},{6,8,2,4,-5},{9,5,7,3,6},{33,75,4322,472,-475},{-43,-65,437,-2222,1289}};
+        System.out.println(test.length);
+        double result = PolyReg.determinantOfMatrix(test,1);
+        System.out.println(result + "\n\n");
+        for(double[] x : PolyReg.findInverseMatrices(test))
+        {
+            for(double y : x)
+            {
+                System.out.print(y + " ");
+            }
+            System.out.println();
+        }
         
     }
 }
