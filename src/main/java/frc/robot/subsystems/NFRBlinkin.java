b4ade793631cf3d3279bcce5dbb6edaaee1f63fb
Kasey Nanovic <148725607+kcnan@users.noreply.github.com>
2024-04-01 10:37:23 -0400

    made NFRBlinkin only accept WPILib's preset Colors (via strings)

--- src/main/java/frc/robot/subsystems/NFRBlinkin.java
+++ src/main/java/frc/robot/subsystems/NFRBlinkin.java
@@ -24,36 +24,37 @@ public class NFRBlinkin extends NFRSubsystem implements NFRLED {
         }
     }
     protected final Spark blinkinSpark;
-    public static final Map<Color, Double> colorMap = Map.ofEntries(
+    public static final Map<String, Double> colorMap = Map.ofEntries(
         // https://www.revrobotics.com/content/docs/REV-11-1105-UM.pdf
-        Map.entry(Color.kHotPink,    .57),
-        Map.entry(Color.kDarkRed,    .59),
-        Map.entry(Color.kRed,        .61),
-        Map.entry(Color.kOrangeRed,  .63),
-        Map.entry(Color.kOrange,     .65),
-        Map.entry(Color.kGold,       .67),
-        Map.entry(Color.kYellow,     .69),
-        Map.entry(Color.kLawnGreen,  .71),
-        Map.entry(Color.kLime,       .73),
-        Map.entry(Color.kDarkGreen,  .75),
-        Map.entry(Color.kGreen,      .77),
+        // strings so only the exact preset colors are chosen
+        Map.entry("kHotPink",    .57),
+        Map.entry("kDarkRed",    .59),
+        Map.entry("kRed",        .61),
+        Map.entry("kOrangeRed",  .63),
+        Map.entry("kOrange",     .65),
+        Map.entry("kGold",       .67),
+        Map.entry("kYellow",     .69),
+        Map.entry("kLawnGreen",  .71),
+        Map.entry("kLime",       .73),
+        Map.entry("kDarkGreen",  .75),
+        Map.entry("kGreen",      .77),
         // Blue Green      .79
-        Map.entry(Color.kAqua,       .81),
-        Map.entry(Color.kSkyBlue,    .83),
-        Map.entry(Color.kDarkBlue,   .85),
-        Map.entry(Color.kBlue,       .87),
-        Map.entry(Color.kBlueViolet, .89),
-        Map.entry(Color.kViolet,     .91),
-        Map.entry(Color.kWhite,      .93),
-        Map.entry(Color.kGray,       .95),
-        Map.entry(Color.kDarkGray,   .97),
-        Map.entry(Color.kBlack,      .99));
+        Map.entry("kAqua",       .81),
+        Map.entry("kSkyBlue",    .83),
+        Map.entry("kDarkBlue",   .85),
+        Map.entry("kBlue",       .87),
+        Map.entry("kBlueViolet", .89),
+        Map.entry("kViolet",     .91),
+        Map.entry("kWhite",      .93),
+        Map.entry("kGray",       .95),
+        Map.entry("kDarkGray",   .97),
+        Map.entry("kBlack",      .99));
     public NFRBlinkin(NFRBlinkinConfiguration config) {
         super(config);
         this.blinkinSpark = new Spark(config.blinkinID);
     }
     public double mapColor(Color color) {
-        return (colorMap.get(color) == null) ? colorMap.get(color) : .93;
+        return colorMap.containsKey(color.toString()) ? colorMap.get(color.toString()) : .93;
     }
     public void setColor(Color color) {
         blinkinSpark.set(mapColor(color));
