12d092d098a654c044f9ff752a4bed134e11e4bb
Kasey Nanovic <148725607+kcnan@users.noreply.github.com>
2024-04-01 19:59:42 -0400

    LEDRelay subsystem

---
+++ src/main/java/frc/robot/subsystems/LEDRelay.java
@@ -0,0 +1,20 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.Relay;
+import edu.wpi.first.wpilibj.Relay.Direction;
+import edu.wpi.first.wpilibj.Relay.Value;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class LEDRelay extends SubsystemBase
+{
+    Relay relay;
+    public LEDRelay(int relayChannel)
+    {
+        super("LEDRelay");
+        this.relay = new Relay(relayChannel, Direction.kForward);
+    }
+    public void setRelay(boolean state)
+    {
+        relay.set(state ? Value.kOn : Value.kOff);
+    }
+}
